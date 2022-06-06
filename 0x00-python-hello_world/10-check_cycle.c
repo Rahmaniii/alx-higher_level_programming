7f22325352fc7a1aa8d59565f05e27fb4b0d56c7
Rahmaniii <i.raymon57@gmail.com>
2022-06-06 00:26:09 +0000

    lst

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks cycle of node
+ * @list: pointer to lists
+ *
+ * Return: Always 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *list;
+	int n;
+
+	n = 0;
+	if (list == NULL)
+		return (0);
+	else
+		return(1);;
+}
